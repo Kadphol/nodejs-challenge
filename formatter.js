b8b88d7c4dc79e63394b573faafbe3f621ed7fd7
Kadphol <kadphol.len@outlook.co.th>
2022-03-23 22:41:10 +0700

    feat: Add JSON format function

---
+++ formatter.js
@@ -0,0 +1,14 @@
+exports.fomatter = (payload) => {
+    let array = [];
+    for(let key in payload) {
+        for(let obj of payload[key]) {
+            array.push(obj);
+        }
+    }
+    array.forEach(e => {
+        if(e.parent_id) {
+            array.find(p => p.id === e.parent_id).children.push(e);
+        }
+    });
+    return array.filter(el => el.parent_id == null);
+}
