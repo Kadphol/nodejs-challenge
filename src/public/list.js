3116ca19bcb930fc870ca99132eb89db48bf3dd3
Kadphol <kadphol.len@outlook.co.th>
2022-03-27 19:06:37 +0700

    fix: change index

---
+++ src/public/list.js
@@ -0,0 +1,40 @@
+$(function() {
+    (function(name) {
+        var container = $('#pagination-' + name);
+        container.pagination({
+            dataSource: function(done) {
+                $.ajax({
+                    type: 'GET',
+                    url: `/api/github/all`,
+                    beforeSend: function() {
+                        container.prev().html('Loading data from github ...');
+                    },
+                    success: function(response) {
+                        done(response.items);
+                    }
+                });
+            },
+            locator: 'items',
+            pageSize: 10,
+            pageRange: 10,
+            pageNumber: 1,
+            showPageNumbers: true,
+            showPrevious: true,
+            showNext: true,
+            showNavigator: true,
+            showFirstOnEllipsisShow: true,
+            showLastOnEllipsisShow: true,
+            callback: function(response, pagination) {
+                var dataHtml = '<ul>';
+
+                $.each(response, function (index, item) {
+                    dataHtml += '<li><a href="' + item.html_url + '">' + item.full_name + '</a></li>';
+                });
+
+                dataHtml += '</ul>';
+
+                container.prev().html(dataHtml);
+            }
+        });
+    })('demo2');
+})
